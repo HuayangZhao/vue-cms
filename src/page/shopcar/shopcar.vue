6cfd48526e5fbc0a3018da6bf1b22cadc50dd99d
Mr.Zhy <LiZhenShuai@handsome.com>
2018-11-30 23:19:24 +0800

    完成商品勾选状态组件之间的数据联动

--- src/page/shopcar/shopcar.vue
+++ src/page/shopcar/shopcar.vue
@@ -30,7 +30,10 @@
                 <div class="mui-card-content-inner jiesuan">
                     <div class="left">
                         <p>总计（不含运费）</p>
-                        <p>已勾选商品 <span class="red">5</span> 件， 总价 <span class="red">￥4444</span></p>
+                        <p>已勾选商品 
+                            <span class="red">{{$store.getters.getNumTrue.num}}</span> 件， 总价 
+                            <span class="red">￥{{$store.getters.getNumTrue.total}}</span>
+                        </p>
                      </div>
                     <mt-button type="danger">去结算</mt-button>
                 </div>
